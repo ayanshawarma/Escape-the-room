b2769e3a03282a89046d997bc7efd87456605a4f
Ayan Sharma <42250040+FireDragon6128@users.noreply.github.com>
2020-08-23 10:49:50 +0530

    Add files via upload

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Owner = GetOwner();
+	
+}
+
+void UOpenDoor::OpenDoor()
+{
+	//create rotator
+	FRotator NewRotator = FRotator(0.f, -80.f, 0.f);
+
+	//set door rotation
+	Owner->SetActorRotation(NewRotator);
+}
+
+void UOpenDoor::CloseDoor()
+{
+	//create rotator
+	FRotator NewRotator = FRotator(0.f, 0.f, 0.f);
+
+	//set door rotation
+	Owner->SetActorRotation(NewRotator);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (GetTotalMassOfActorsOnPlate() >= 10.f) {
+		OpenDoor();
+		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+	}
+
+	if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay) {
+		CloseDoor();
+	}
+}
+
+float UOpenDoor::GetTotalMassOfActorsOnPlate()
+{
+	float TotalMass = 0.f;
+	TArray<AActor*> OverlappingActors;
+	PressurePlate->GetOverlappingActors(OverlappingActors);
+
+	for (const auto& Actor : OverlappingActors) {
+		TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+		UE_LOG(LogTemp, Warning, TEXT("%s on pressure plate"), *Actor->GetName());
+	}
+
+	return TotalMass;
+}
+
